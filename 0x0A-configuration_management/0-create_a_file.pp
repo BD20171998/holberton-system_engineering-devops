47bf57f443a534d564ec9144e1119abdc75de600
Bob Deprizio <robert.deprizio@gmail.com>
2020-03-23 23:31:29 +0000

    Fixed typo in CONTENT string

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,7 +2,7 @@
 file { '/tmp/holberton':
   ensure  => 'present',
   path    => '/tmp/holberton',
-  content => 'I love puppet',
+  content => 'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
