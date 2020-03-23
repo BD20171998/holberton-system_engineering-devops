a74601cbbab0368ea9a0ee07c4670f3ab45e8645
Bob Deprizio <robert.deprizio@gmail.com>
2020-03-23 23:16:20 +0000

    Added PATH for puppet file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,5 +1,6 @@
 # Using Puppet, create a file in /tmp
-file { '/tmp/holberton':
+file { 'holberton':
+  path    => '/tmp/holberton',
   ensure  => 'present',
   content => 'I love puppet',
   mode    => '0744',
