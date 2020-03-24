e9a74131b81d2d4f0ca1f59cf06fe7a3a8d52ae6
Bob Deprizio <robert.deprizio@gmail.com>
2020-03-24 01:17:52 +0000

    Adding puppet manifest

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+# Using Puppet, create a manifest that kills a process named killmenow
+exec { 'killmenow':
+  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command  => 'pkill killmenow',
+  provider => 'shell',
+}
