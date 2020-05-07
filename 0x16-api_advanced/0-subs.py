2afa86b5296fc721c4dc7488080ec0b3d523f007
Bob Deprizio <robert.deprizio@gmail.com>
2020-05-07 23:49:21 +0000

    Added try/except statement

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,16 +12,16 @@ def number_of_subscribers(subreddit):
     user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129 '}
     url = 'https://www.reddit.com/r/' + subreddit + '.json'
 
-    response = get(url, headers=user_agent)
-    all_data = response.json()
+    try:
+        response = get(url, headers=user_agent)
+        all_data = response.json()
 
-    raw_data = all_data['data']
-    raw_data2 = raw_data['children']
+        raw_data = all_data['data']
+        raw_data2 = raw_data['children']
 
-    total = raw_data2[0]['data']['subreddit_subscribers']
+        total = raw_data2[0]['data']['subreddit_subscribers']
 
-    if total is None:
-        return 0
-
-    else:
         return total
+
+    except:
+        return 0
