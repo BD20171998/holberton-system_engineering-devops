5ae5bcb65b5b150c6e2e140b0f095017512586b5
Bob Deprizio <robert.deprizio@gmail.com>
2020-05-08 04:00:39 +0000

    Adding Python script

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+from requests import get
+
+
+def top_ten(subreddit):
+    """
+    function that queries the Reddit API and prints the titles of the first
+    10 hot posts listed for a given subreddit
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        print("None")
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    params = {'limit': 10}
+    url = 'https://www.reddit.com/r/{}/top/.json'.format(subreddit)
+
+    response = get(url, headers=user_agent, params=params)
+    all_data = response.json()
+
+    try:
+        raw1 = all_data.get('data').get('children')
+
+        for i in raw1:
+            print(i.get('data').get('title'))
+
+    except:
+        print("None")
