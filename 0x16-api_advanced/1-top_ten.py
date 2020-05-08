7197f8af949142a1d11cddd1efc011d23efa0749
Bob Deprizio <robert.deprizio@gmail.com>
2020-05-08 04:17:17 +0000

    Changed url

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -14,7 +14,7 @@ def top_ten(subreddit):
 
     user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     params = {'limit': 10}
-    url = 'https://www.reddit.com/r/{}/top/.json'.format(subreddit)
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
 
     response = get(url, headers=user_agent, params=params)
     all_data = response.json()
