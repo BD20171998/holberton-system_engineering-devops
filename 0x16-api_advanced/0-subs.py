498e45a215e6f7ac8a498043b77ba872ac4e1594
Bob Deprizio <robert.deprizio@gmail.com>
2020-05-08 05:31:58 +0000

    Consolidated commands in try section

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,14 +12,13 @@ def number_of_subscribers(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129 '}
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = get(url, headers=user_agent)
     all_data = response.json()
 
     try:
-        raw_data = all_data.get('data')
-        return (raw_data.get('subscribers'))
+        return all_data.get('data').get('subscribers')
 
     except:
         return 0
