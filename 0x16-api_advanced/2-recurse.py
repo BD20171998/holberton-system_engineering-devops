d9fce649ffe558eda9609a433150c485440df375
Bob Deprizio <robert.deprizio@gmail.com>
2020-05-11 03:09:10 +0000

    Adding Python script

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+"""
+importing requests module
+"""
+
+from requests import get
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """
+    function that queries the Reddit API and returns a list containing the
+    titles of all hot articles for a given subreddit.
+    """
+
+    params = {'show': 'all'}
+
+    if subreddit is None or not isinstance(subreddit, str):
+        print("None")
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+
+    url = 'https://www.reddit.com/r/{}/hot/.json?after={}'.format(subreddit,
+                                                                  after)
+
+    response = get(url, headers=user_agent, params=params)
+    all_data = response.json()
+
+    try:
+        raw1 = all_data.get('data').get('children')
+        after = all_data.get('data').get('after')
+
+        if after is None:
+            return hot_list
+
+        for i in raw1:
+            hot_list.append(i.get('data').get('title'))
+
+        return recurse(subreddit, hot_list, after)
+    except:
+        print("None")
