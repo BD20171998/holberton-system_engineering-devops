821e091dabb641da4a7717f573e0260aaf558068
Bob Deprizio <robert.deprizio@gmail.com>
2020-05-19 23:07:32 +0000

    Adding Puppet file and README

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+#Using strace to find out why Apache is returning a 500 error and then using
+#Puppet to automate fix
+exec { 'fix_typo':
+  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command  =>  'sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g"
+  /var/www/html/wp-settings.php',
+  provider =>  'shell'}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,14 @@
+# 0x17. Web stack debugging #3
+
+## Description
+What you should learn from this project:
+
+---
+
+### [0. Strace is your friend](./0-strace_is_your_friend.pp)
+* 
+
+---
+
+## Author
+* **Robert Deprizio** - [BD20171998](https://github.com/BD20171998)
