f01b58e53ddc52b68bbfa8f8fc8691ba6dfc4c9f
Bob Deprizio <robert.deprizio@gmail.com>
2020-05-25 05:54:46 +0000

    Removed 'shell' for provider

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -4,4 +4,4 @@ exec { 'fix_typo':
   path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
   command  =>  "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g'
   /var/www/html/wp-settings.php",
-  provider =>  'shell'}
+  }
