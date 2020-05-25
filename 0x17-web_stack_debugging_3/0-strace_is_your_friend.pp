571d0220bd9b379b57cc5be1647b8dbeec457f31
Bob Deprizio <robert.deprizio@gmail.com>
2020-05-25 05:57:46 +0000

    Removed multiple paths from array for path attribute

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 #Using strace  and then using Puppet to automate fix for 500 error on Apache
 
 exec { 'fix_typo':
-  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  path     => '/bin',
   command  =>  "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g'
   /var/www/html/wp-settings.php",
   }
