25a38a982b895a1e2208e19cd02c49d98de2e54e
Bob Deprizio <robert.deprizio@gmail.com>
2020-05-25 05:47:09 +0000

    Added blank space between comment and code

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 #Using strace to find out why Apache is returning a 500 error and then using
 #Puppet to automate fix
+
 exec { 'fix_typo':
   path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
   command  =>  'sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g"
